e037b838f781b2d0f40daa1f82915b008809a584
SimoneVilardo <simovila95@gmail.com>
2023-06-01 14:32:07 +0200

    SNACK 1 TERMINATO

---
+++ snack1/js/script.js
@@ -0,0 +1,21 @@
+const invitati = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
+const segnaposto = invitati.map((item, index) => {
+
+    let oggetti ={
+        tavolo: 'Tavolo Vip',
+        name: item,
+        value: index + 1
+    }
+
+    return oggetti
+})
+
+
+for(let i=0; i<segnaposto.length; i++){
+    let risultato_segnaposto = segnaposto[i];
+    console.log(risultato_segnaposto.tavolo)
+    console.log(risultato_segnaposto.name)
+    console.log(risultato_segnaposto.value)
+
+}
